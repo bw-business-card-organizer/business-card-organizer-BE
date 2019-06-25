70b15bb785585f0d69ae2bcf1bdcc60da9517116
Raine Wallace <rainebeaudubs@gmail.com>
2019-06-25 17:58:29 -0400

    put request is now functional

--- routes/cards-router.js
+++ routes/cards-router.js
@@ -16,9 +16,9 @@ router.get('/', authenticate, async (req, res) => {
       .status(500)
       .json({
         message: 'There was an error retreiving these cards.'
-      })
+      });
   }
-})
+});
 
 router.post('/', authenticate, async(req, res) => {
   try {
@@ -31,8 +31,31 @@ router.post('/', authenticate, async(req, res) => {
       .status(500)
       .json({
         message: 'There was an error adding this card.'
-      })
+      });
   }
-})
+});
+
+router.put('/:id', authenticate, async (req, res) => {
+  try {
+    const card = await Cards.update(req.params.id, req.body);
+    if (card) {
+      res
+        .status(200)
+        .json(card);
+    } else {
+      res
+        .status(404)
+        .json({
+          message: "This card could not be found"
+        });
+    }
+  } catch (error) {
+    res
+      .status(500)
+      .json({
+        message: "There was an error updating this card."
+      });
+  }
+});
 
 module.exports = router;
