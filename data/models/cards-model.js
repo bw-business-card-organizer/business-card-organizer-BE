2c9c5250f247b426b318d8e9f9dd11c6ba459832
Raine Wallace <rainebeaudubs@gmail.com>
2019-06-25 13:52:32 -0400

    created simple cards-model

---
+++ data/models/cards-model.js
@@ -0,0 +1,50 @@
+const db = require('../dbConfig');
+
+module.exports = {
+  find,
+  findBy,
+  findById,
+  add,
+  update,
+  remove
+};
+
+function find() {
+  return db('bizCards');
+}
+
+function findBy(filter) {
+  return db('bizCards')
+    .where(filter)
+}
+
+function findById(id) {
+  return db('bizCards')
+    .select('id')
+    .where({
+      id
+    })
+    .first();
+}
+
+function add(card) {
+  return db('bizCards')
+    .insert(card)
+    .then(ids => {
+      return getById(ids[0]);
+    });
+}
+
+function update(id, changes) {
+  return db('bizCards')
+    .where({ 
+      id 
+    })
+    .update(changes);
+}
+
+function remove(id) {
+  return db('bizCards')
+    .where('id', id)
+    .del();
+}
