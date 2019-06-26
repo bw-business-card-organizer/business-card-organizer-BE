e168d7e85811e5e1c7a73819038f1ab9f2f0156a
Raine Wallace <rainebeaudubs@gmail.com>
2019-06-26 11:13:12 -0400

    face-palm

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -3,4 +3,4 @@ const knexConfig = require('../knexfile.js');
 
 const enviornment = process.env.DB_ENV || 'development';
 
-module.exports = knex(knexConfig.development);
+module.exports = knex(knexConfig[enviornment]);
