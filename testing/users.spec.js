866e16714dc69c6004587e397a3658d0a6a4e027
Raine Wallace <rainebeaudubs@gmail.com>
2019-06-27 04:47:55 -0400

    3 tests passing, not sure if those are evn the results I want, but hey, they're passing

---
+++ testing/users.spec.js
@@ -0,0 +1,31 @@
+const request = require('supertest');
+const server = require('../api/server');
+
+describe('POST /api/register', () => {
+  it('should post user as text/html', async () => {
+    const response = await request(server).post('/api/register');
+
+    expect(response.type).toBe('text/html');
+  });
+});
+
+describe('POST /api/login', () => {
+  it('should post user as JSON', async () => {
+    const response = await request(server).post('/api/login');
+
+    expect(response.type).toBe('application/json');
+  })
+
+  it('should return a status 401 when email is not found', async () => {
+    let user = {
+      email: 'notAtAllAnEmail',
+      password: 'mittsey'
+    }
+    const response = await request(server)
+      .post('/api/login')
+      .send(user)
+
+    expect(response.status).toEqual(401);
+  });
+  
+});
